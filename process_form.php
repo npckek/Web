c450a79793323159dff999bf2ca01047c40f8f58
Arsenii T <45126830+npckek@users.noreply.github.com>
2024-04-19 12:25:07 +0300

    Update process_form.php
    Removed all comments

--- process_form.php
+++ process_form.php
@@ -2,9 +2,6 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     
-    // Process data (e.g., save to database, etc.)
-    
-    // Return a response
     echo "Hello, $username! Your data has been received on the server.";
 }
 ?>
