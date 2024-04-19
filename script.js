6515cbcf618fb8a40188c60a7df50bdc42a66e21
Arsenii T <45126830+npckek@users.noreply.github.com>
2024-04-19 12:27:05 +0300

    Update script.js
    Remove all comments

--- script.js
+++ script.js
@@ -3,19 +3,16 @@ document.getElementById('changeContentBtn').addEventListener('click', function()
 });
 
 document.getElementById('userForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault();
 
-    // Client-side validation
     var username = document.getElementById('username').value;
     if (username.trim() === '') {
         document.getElementById('validationMessage').innerText = 'Please enter your name.';
         return;
     }
 
-    // If validation passes, reset validation message
     document.getElementById('validationMessage').innerText = 'Упс... Теперь я знаю как вас зовут.';
 
-    // Simulate sending data to server
     var formData = new FormData(this);
     fetch('process_form.php', {
         method: 'POST',
@@ -23,7 +20,7 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
     })
     .then(response => response.text())
     .then(data => {
-        console.log(data); // Log response from server
+        console.log(data); 
     })
     .catch(error => {
         console.error('Error:', error);
